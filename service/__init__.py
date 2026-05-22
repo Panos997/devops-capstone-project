a9ac86fb5d0b577a871360a4e9c8082a90debdb0
Panos Antoniozas <145284714+Panos997@users.noreply.github.com>
2026-05-22 15:56:44 +0300

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,9 +1,4 @@
-"""
-Package: service
-Package for the application models and service routes
-This module creates and configures the Flask app and sets up the logging
-and SQL database
-"""
+
 import sys
 from flask import Flask
 from service import config
